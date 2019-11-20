73f09bc12185417575adb43b528803fc2567b649
AdamRohdell <rohdell@student.chalmers.se>
2019-11-20 17:08:57 +0100

    Almost finished with part 3

--- Lab2/src/Autocomplete.java
+++ Lab2/src/Autocomplete.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Autocomplete {
     // Initializes the data structure from the given array of terms.
     // Complexity: O(N log N), where N is the number of terms
@@ -7,20 +9,26 @@ public class Autocomplete {
         this.terms = terms;
         c = Term.byLexicographicOrder();
 
-
+        quickSort(this.terms, 0, this.terms.length-1);
         
     }
 
     // Returns all terms that start with the given prefix, in descending order of weight.
     // Complexity: O(log N + M log M), where M is the number of matching terms
     public Term[] allMatches(String prefix){
-
     }
 
     // Returns the number of terms that start with the given prefix.
     // Complexity: O(log N)
     public int numberOfMatches(String prefix){
+        c = Term.byPrefixOrder(prefix.length());
+        int ret = 0;
+        Term t = new Term(prefix, 1);
+        if (RangeBinarySearch.firstIndexOf(terms, t, c)> -1){
+            ret++;
+        }
 
+        return ret;
     }
 
 
@@ -48,9 +56,9 @@ public class Autocomplete {
             }
         }
 
-        Temp temp = terms[low];
+        Term temp = terms[low];
         terms[low] = pivot;
-        terms[high] = pivot;
+        terms[high] = temp;
  
         return low;
     }
