0902dcc957f10a8f239e9d9ddbc271777e5d0775
Amanda <amandadehlen@gmail.com>
2019-11-20 10:19:36 +0100

    Update Term.java

--- Lab2/src/Term.java
+++ Lab2/src/Term.java
@@ -1,2 +1,23 @@
 public class Term {
+    // Initializes a term with the given query string and weight.
+    public Term(String query, long weight)
+
+    // Compares the two terms in lexicographic order by query.
+    public static Comparator<Term> byLexicographicOrder()
+
+    // Compares the two terms in descending order by weight.
+    public static Comparator<Term> byReverseWeightOrder()
+
+    // Compares the two terms in lexicographic order,
+    // but using only the first k characters of each query.
+    public static Comparator<Term> byPrefixOrder(int k)
+
+    // Returns a string representation of this term in the following format:
+    // the weight, followed by whitespace, followed by the query.
+    public String toString() {
+        return String.format("%12d    %s", this.weight, this.query);
+    }
+
+
+
 }
