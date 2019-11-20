359cb0075c0bb2b2940d579adef2ac3d93fe0f31
AdamRohdell <rohdell@student.chalmers.se>
2019-11-20 16:04:03 +0100

    soloution for part 2

--- Lab2/src/RangeBinarySearch.java
+++ Lab2/src/RangeBinarySearch.java
@@ -1,9 +1,52 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class RangeBinarySearch {
     // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
     // Complexity: O(log N), where N is the length of the array
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator)
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+        if (a == null || key == null || comparator == null){
+            throw new NullPointerException();
+        }
+        int low = 0;
+        int high = a.length-1;
+        int mid = (low+high)/2;
+        int currentIndexOfKey = -1;
+        while (low < high){
+            if (comparator.compare(key, a[mid]) > 0){
+                low = mid + 1;
+            } else if (comparator.compare(key, a[mid]) < 0){
+                high = mid - 1; 
+            } else {
+                currentIndexOfKey = mid;
+                high = mid - 1;
+            }
+            mid  = (low + high)/2;
+        }
+        return currentIndexOfKey;
+    }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     // Complexity: O(log N)
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator)
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator){
+        if (a == null || key == null || comparator == null){
+            throw new NullPointerException();
+        }
+        int low = 0;
+        int high = a.length-1;
+        int mid = (low+high)/2;
+        int currentIndexOfKey = -1;
+        while (low < high){
+            if (comparator.compare(key, a[mid]) > 0){
+                low = mid + 1;
+            } else if (comparator.compare(key, a[mid]) < 0){
+                high = mid - 1; 
+            } else {
+                currentIndexOfKey = mid;
+                low = mid + 1;
+            }
+            mid  = (low + high)/2;
+        }
+        return currentIndexOfKey;
+    }
 }
