49c34eb5d5aa1891d29531aa1caad7ce71177d45
Amanda <amandadehlen@gmail.com>
2019-11-21 15:37:30 +0100

    Update Autocomplete.java

--- Lab2/src/Autocomplete.java
+++ Lab2/src/Autocomplete.java
@@ -6,30 +6,43 @@ public class Autocomplete {
     // Complexity: O(N log N), where N is the number of terms
     Term[] terms;
     Comparator<Term> c;
-    public Autocomplete(Term[] terms){
+
+    public Autocomplete(Term[] terms) {
         this.terms = terms;
         c = Term.byLexicographicOrder();
 
         Arrays.sort(terms, c);
-        
+
     }
 
     // Returns all terms that start with the given prefix, in descending order of weight.
     // Complexity: O(log N + M log M), where M is the number of matching terms
-    public Term[] allMatches(String prefix){
+    public Term[] allMatches(String prefix) {
+        Term[] ret = new Term[numberOfMatches(prefix)];
+        Term t = new Term(prefix, 1);
+        int pos = 0;
+
+        if (ret.length > 0) {
+            for (int i = RangeBinarySearch.firstIndexOf(terms, t, c); i <= RangeBinarySearch.lastIndexOf(terms, t, c); i++) {
+                ret[pos] = terms[i];
+                pos++;
+            }
+        }
+        return ret;
     }
 
     // Returns the number of terms that start with the given prefix.
     // Complexity: O(log N)
-    public int numberOfMatches(String prefix){
+    public int numberOfMatches(String prefix) {
         c = Term.byPrefixOrder(prefix.length());
         int ret = 0;
         Term t = new Term(prefix, 1);
-        if (RangeBinarySearch.firstIndexOf(terms, t, c)> -1){
-            ret++;
-        }
 
+        if (RangeBinarySearch.firstIndexOf(terms, t, c) > -1) {
+            for (int i = RangeBinarySearch.firstIndexOf(terms, t, c); i <= RangeBinarySearch.lastIndexOf(terms, t, c); i++) {
+                ret++;
+            }
+        }
         return ret;
     }
- 
 }
